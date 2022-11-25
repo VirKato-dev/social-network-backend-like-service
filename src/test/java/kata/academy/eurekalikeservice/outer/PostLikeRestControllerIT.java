a3e361ebc026d50c83c7c7afeec95a7f189f56f3
VirKato <dascat79@mail.ru>
2022-11-25 12:59:47 +0700

    развернул static import MockMvcRequestBuilders

--- src/test/java/kata/academy/eurekalikeservice/outer/PostLikeRestControllerIT.java
+++ src/test/java/kata/academy/eurekalikeservice/outer/PostLikeRestControllerIT.java
@@ -14,7 +14,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @MockBeans({
